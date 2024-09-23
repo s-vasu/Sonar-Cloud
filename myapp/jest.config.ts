1b35bd1ab0e531aec281393520ff8fa9a0809050
s-vasu <vasu.suryavamsham@zemosolabs.com>
2024-09-24 00:54:51 +0530

    new edits

--- myapp/jest.config.ts
+++ myapp/jest.config.ts
@@ -4,21 +4,29 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   collectCoverage: true,
+
+  // Collect coverage for these files (include)
   collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/components/**/*.{types,stories,constants,test,spec}.{ts,tsx}",
-    "!src/theme/**",
-    "!src/declarations.d.ts",
-    "!src/utils/constant.tsx",
-    "!src/App.tsx",
-    "!src/index.tsx",
-    "!src/reportWebVitals.ts",
-    "!src/store/**",
-    "!src/services/**",
+    "src/**/*.{ts,tsx}", // Include all TypeScript and TSX files in src
   ],
+
+  // Exclude the following files from coverage
+  coveragePathIgnorePatterns: [
+    "src/components/**/*.{types,stories,constants,test,spec}.{ts,tsx}", // Exclude component-related types, stories, constants, tests, and specs
+    "src/theme/**", // Exclude theme files
+    "src/declarations.d.ts", // Exclude TypeScript declaration files
+    "src/utils/constant.tsx", // Exclude specific utility file
+    "src/App.tsx", // Exclude main app file
+    "src/index.tsx", // Exclude index entry point
+    "src/reportWebVitals.ts", // Exclude reportWebVitals file
+    "src/store/**", // Exclude store directory
+    "src/services/**", // Exclude services directory
+  ],
+
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS module imports
   },
 };
 
